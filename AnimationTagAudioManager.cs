e5b8ed63d4ceafa29dc809329a36fb7dfcbe6d0e
Chris "BØLT" Bolte <124454306+bolttracks@users.noreply.github.com>
2024-12-10 17:16:16 -0500

    Simple universal Wwise Audio Manager
    Use it to easily set up complex animation tagging workflows that allow calling any number of Wwise events on a given Game Object.

---
+++ AnimationTagAudioManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTagAudioManager : MonoBehaviour
+{
+    //Replace AnimTag with the desired name for your property drawers
+    [Header("Section A")]
+    public AK.Wwise.Event AnimTag1;
+    public AK.Wwise.Event AnimTag2;
+    public AK.Wwise.Event AnimTag3;
+    public AK.Wwise.Event AnimTag4;
+
+    [Header("Section B")]
+    public AK.Wwise.Event AnimTag5;
+    public AK.Wwise.Event AnimTag6;
+    public AK.Wwise.Event AnimTag7;
+
+    //Choose a descriptive header name (or mirror names above)
+    #region SectionA
+
+    void animTag1()
+    {
+        AnimTag1.Post(gameObject);
+    }
+
+    void animTag2()
+    {
+        AnimTag2.Post(gameObject);
+    }
+
+    void animTag3()
+    {
+        AnimTag3.Post(gameObject);
+    }
+
+    void animTag4()
+    {
+        AnimTag4.Post(gameObject);
+    }
+
+    #endregion
+
+
+    #region SectionB
+
+    void animTag5()
+    {
+        AnimTag5.Post(gameObject);
+    }
+
+    void animTag6()
+    {
+        AnimTag6.Post(gameObject);
+    }
+
+    void animTag7()
+    {
+        AnimTag7.Post(gameObject);
+    }
+
+    #endregion
+
+}
